0f606512f16983ac65eb004a34db4e0a1bfdd4ab
Lu Han <feuyeux@gmail.com>
2024-12-16 19:40:51 +0800

    plan-and-execute

---
+++ hello_pne.py
@@ -0,0 +1,228 @@
+import asyncio
+import io
+import operator
+import os
+from typing import Annotated, List, Tuple
+from typing import Union
+
+from PIL import Image as PilImage
+from dotenv import load_dotenv
+from langchain import hub
+from langchain_community.chat_models import ChatZhipuAI
+from langchain_community.tools import DuckDuckGoSearchResults
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+from langgraph.graph import END
+from langgraph.graph import StateGraph, START
+from langgraph.prebuilt import create_react_agent
+from pydantic import BaseModel, Field
+from typing_extensions import TypedDict
+
+load_dotenv()
+
+# Define the models
+llama_model = ChatOpenAI(
+    model="llama3.3", base_url="http://localhost:11434/v1")
+zhipu_model = ChatZhipuAI(model="GLM-4-Plus")
+kimi_model = ChatOpenAI(
+    model="moonshot-v1-8k",
+    api_key=os.environ["MOONSHOT_API_KEY"],
+    base_url="https://api.moonshot.cn/v1",
+)
+llms = [llama_model, zhipu_model]
+
+# Define the tools
+# https://api.tavily.com
+tavily = TavilySearchResults(max_results=3)
+
+# https://duckduckgo.com/
+duck_duck_go = DuckDuckGoSearchResults(max_results=3, output_format="json")
+
+search_tools = [tavily]
+
+# Define the executor agent
+
+# https://smith.langchain.com/hub
+# https://smith.langchain.com/hub/ih/ih-react-agent-executor
+# prompt = hub.pull("ih/ih-react-agent-executor")
+# prompt.pretty_print()
+
+def state_modifier(messages: list):
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are a helpful assistant."),
+        MessagesPlaceholder("messages")
+    ])
+    return prompt.invoke({"messages": messages})
+
+
+agent_executor = create_react_agent(
+    model=llms[0],
+    tools=search_tools,
+    state_modifier=state_modifier
+)
+
+
+# execute_result = agent_executor.invoke(
+#     {"messages": [("user", "What is the league ranking of the club that the World Cup champion captain has played for in the past three years?")]})
+# print("execute_result", execute_result)
+
+
+# Define the plan agent
+
+class Plan(BaseModel):
+    """Plan to follow in future"""
+    steps: List[str] = Field(
+        description="different steps to follow, should be in sorted order"
+    )
+
+
+planner_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """For the given objective, come up with a simple step by step plan. \
+This plan should involve individual tasks, that if executed correctly will yield the correct answer. Do not add any superfluous steps. \
+The result of the final step should be the final answer. Make sure that each step has all the information needed - do not skip steps.""",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+)
+planner = planner_prompt | llms[0].with_structured_output(Plan)
+
+planner_result = planner.invoke({
+    "messages": [
+        ("user",
+         "What is the league ranking of the club that the World Cup champion captain has played for in the past three years?")
+    ],
+    "steps": ["Get World Cup Champion", "Get League Ranking"]
+})
+print("planner_result", planner_result)
+
+
+# Define the replan agent
+
+class Response(BaseModel):
+    """Response to user."""
+
+    response: str
+
+
+class Act(BaseModel):
+    """Action to perform."""
+
+    action: Union[Response, Plan] = Field(
+        description="Action to perform. If you want to respond to user, use Response. "
+                    "If you need to further use tools to get the answer, use Plan."
+    )
+
+
+replanner_prompt = ChatPromptTemplate.from_template(
+    """For the given objective, come up with a simple step by step plan. \
+This plan should involve individual tasks, that if executed correctly will yield the correct answer. Do not add any superfluous steps. \
+The result of the final step should be the final answer. Make sure that each step has all the information needed - do not skip steps.
+
+Your objective was this:
+{input}
+
+Your original plan was this:
+{plan}
+
+You have currently done the follow steps:
+{past_steps}
+
+Update your plan accordingly. If no more steps are needed and you can return to the user, then respond with that. Otherwise, fill out the plan. Only add steps to the plan that still NEED to be done. Do not return previously done steps as part of the plan."""
+)
+
+replanner = replanner_prompt | llms[0].with_structured_output(Act)
+
+
+# Define the workflow
+
+
+class PlanExecute(TypedDict):
+    input: str
+    plan: List[str]
+    past_steps: Annotated[List[Tuple], operator.add]
+    response: str
+
+
+async def execute_step(state: PlanExecute):
+    plan = state["plan"]
+    plan_str = "\n".join(f"{i + 1}. {step}" for i, step in enumerate(plan))
+    task = plan[0]
+    task_formatted = f"""For the following plan:
+{plan_str}\n\nYou are tasked with executing step {1}, {task}."""
+    agent_response = await agent_executor.ainvoke(
+        {"messages": [("user", task_formatted)]}
+    )
+    return {
+        "past_steps": [(task, agent_response["messages"][-1].content)],
+    }
+
+
+async def plan_step(state: PlanExecute):
+    plan = await planner.ainvoke({"messages": [("user", state["input"])]})
+    return {"plan": plan.steps}
+
+
+async def replan_step(state: PlanExecute):
+    output = await replanner.ainvoke(state)
+    if isinstance(output.action, Response):
+        return {"response": output.action.response}
+    else:
+        return {"plan": output.action.steps}
+
+
+def should_end(state: PlanExecute):
+    if "response" in state and state["response"]:
+        return END
+    else:
+        return "execute_node"
+
+
+workflow = StateGraph(PlanExecute)
+
+# Add the plan node
+workflow.add_node("plan_node", plan_step)
+
+# Add the execution step
+workflow.add_node("execute_node", execute_step)
+
+# Add a replan node
+workflow.add_node("replan_node", replan_step)
+
+workflow.add_edge(START, "plan_node")
+
+# From plan we go to agent
+workflow.add_edge("plan_node", "execute_node")
+
+# From agent, we replan_node
+workflow.add_edge("execute_node", "replan_node")
+
+workflow.add_conditional_edges(
+    "replan_node",
+    # Next, we pass in the function that will determine which node is called next.
+    should_end,
+    ["execute_node", END],
+)
+
+app = workflow.compile()
+
+# image_bytes = app.get_graph(xray=True).draw_mermaid_png()
+# image_stream = io.BytesIO(image_bytes)
+# image = PilImage.open(image_stream)
+# image.show()
+
+config = {"recursion_limit": 10}
+inputs = {"input": "What is the league ranking of the club that the World Cup champion captain has played for in the past three years?"}
+
+
+async def main():
+    async for event in app.astream(inputs, config=config):
+        for k, v in event.items():
+            if k != "__end__":
+                print(v)
+
+
+asyncio.run(main())
